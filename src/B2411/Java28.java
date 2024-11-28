1013d0e89db6b297944e13e3bf45c6b2ea46326d
ìºzionkim <22100130@handong.ac.kr>
2024-11-28 23:38:00 +0900

    241128

---
+++ src/B2411/Java28.java
@@ -0,0 +1,14 @@
+package B2411;
+
+import java.util.*;
+
+public class Java28 {
+    public void solution(){
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void solution(){
+
+    }
+
+    public static void main(String[] args){
+        solution();
+    }
+}
